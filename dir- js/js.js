9bea793c0cac666dcc0f0cfd14ba2b74a6d65210
Maykon Johnsonn <maykonjohnsonn@hotmail.com>
2019-05-30 01:31:01 -0300

    Arquivos teste javaScript

---
+++ dir- js/js.js
@@ -0,0 +1,30 @@
+var mensagem = "Sai dai Cagao";
+var mensagemCompleta = "vai chupar um prego";
+
+
+var digitato = document.getElementById('digi').value;
+
+function chuparPrego(){
+    console.log("A mensagem é: "+mensagem+", "+mensagemCompleta);
+}
+
+function clicouPorra(){
+    console.log(digitado);
+}
+
+
+
+var aluno = {
+    nome: "Zezinho da Silva",
+    idade: 21,
+    curso: "Letras",
+    estudar: function(){
+        console.log("Bora estudar né galera!")
+    }
+};
+
+//console.log(aluno);
+
+aluno.estudar();
+console.log(aluno.nome);
+//aluno.estudar();
